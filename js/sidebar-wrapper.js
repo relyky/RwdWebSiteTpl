d53244255ea2173bb045d855482f6d01dc488b6e
Rely Ky <relyky@hotmail.com>
2017-07-19 18:20:16 +0800

    add MainFrame6A

---
+++ js/sidebar-wrapper.js
@@ -0,0 +1,12 @@
+﻿
+// Closes the sidebar menu
+$("#menu-close").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
